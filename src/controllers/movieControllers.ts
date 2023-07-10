810954e86ea45469a38da6a9ce69bd363f063e5b
Victor Nathanael <victornathanael15@gmail.com>
2023-07-10 11:33:08 -0300

    feat: add findMovieById, getAllMovies, removeMovie and updateMovie

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -1,13 +1,83 @@
 import { Request, Response } from 'express';
-import { MoviewModel } from '../models/Movie';
+import { MovieModel } from '../models/Movie';
 import Logger from '../../config/logger';
 
 export async function createMovie(req: Request, res: Response) {
     try {
         const data = req.body;
-        const movie = await MoviewModel.create(data);
+        const movie = await MovieModel.create(data);
         return res.status(201).json(movie);
     } catch (e: any) {
         Logger.error(`Erro no sistema: ${e.message}`);
+        return res.status(500).json({ error: 'Por favor, tente mais tarde!' });
+    }
+}
+
+export async function findMovieById(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        const movie = await MovieModel.findById(id);
+
+        if (!movie) {
+            return res.status(404).json({ error: 'O filme não existe.' });
+        }
+        return res.status(200).json(movie);
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`);
+        return res.status(500).json({ error: 'Por favor, tente mais tarde!' });
+    }
+}
+
+export async function getAllMovies(req: Request, res: Response) {
+    try {
+        const movies = await MovieModel.find();
+
+        if (movies.length < 1) {
+            return res
+                .status(404)
+                .json({ error: 'Não existem filmes no banco de dados' });
+        }
+
+        return res.status(200).json(movies);
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`);
+        return res.status(500).json({ error: 'Por favor, tente mais tarde!' });
+    }
+}
+
+export async function removeMovie(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        const movie = await MovieModel.findById(id);
+
+        if (!movie) {
+            return res.status(404).json({ error: 'O filme não existe.' });
+        }
+
+        await MovieModel.deleteOne({ _id: id });
+        return res.status(200).send({ msg: 'Filme removido com sucesso!' });
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`);
+        return res.status(500).json({ error: 'Por favor, tente mais tarde!' });
+    }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+    try {
+        const id = req.params.id;
+        const data = req.body;
+
+        const movie = await MovieModel.findById(id);
+
+        if (!movie) {
+            return res.status(404).json({ error: 'O filme não existe.' });
+        }
+
+        await MovieModel.updateOne({ _id: id }, data);
+
+        return res.status(200).send(data);
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`);
+        return res.status(500).json({ error: 'Por favor, tente mais tarde!' });
     }
 }
