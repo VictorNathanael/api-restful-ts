d7c5b6899785871e24d006184f7a67f3a38650a7
Victor Nathanael <victornathanael15@gmail.com>
2023-07-10 11:36:00 -0300

    feat: add routes for /movie and /movie/:id

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,13 @@
 import { Router, Request, Response } from 'express';
-import { createMovie } from './controllers/movieControllers';
+import {
+    createMovie,
+    getAllMovies,
+    findMovieById,
+    removeMovie,
+    updateMovie,
+} from './controllers/movieControllers';
 import { validate } from './middleware/handleValidation';
+import { movieCreateValidation } from './middleware/movieValidation';
 
 const router = Router();
 
@@ -8,4 +15,8 @@ export default router
     .get('/test', (req: Request, res: Response) => {
         res.status(200).send('API Working!');
     })
-    .post('/movie', validate, createMovie);
+    .post('/movie', movieCreateValidation(), validate, createMovie)
+    .get('/movie/:id', findMovieById)
+    .get('/movie', getAllMovies)
+    .delete('/movie/:id', removeMovie)
+    .patch('/movie/:id', movieCreateValidation(), validate, updateMovie);
